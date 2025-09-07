f6fb1709d0bfff535ba237025d5a176c73813934
ZouZixuan666 <zzou0015@student.monash.edu>
2025-09-07 13:33:33 +0800

    Delete app directory

--- app/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    """A base class for all users in the system."""
-    def __init__(self, user_id, name):
-        self.id = user_id
-        self.name = name
-
-    def __repr__(self):
-        return f"<User {self.id}: {self.name}>"
